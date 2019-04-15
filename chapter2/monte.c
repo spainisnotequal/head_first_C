a61efba0f94e4b068defba18ac03309c99a1f78c
spainisnotequal <spainisnotequal@gmail.com>
2019-04-15 10:04:04 +0200

    Add example about string literals, and how we cannot modify them

---
+++ chapter2/monte.c
@@ -0,0 +1,21 @@
+/*
+ * Program that shuffles three cards
+ */
+
+#include <stdio.h>
+
+int main()
+{
+        char *cards = "JQK";
+        char a_card = cards[2];
+
+        cards[2] = cards[1]; // Wrong! String literals cannot be modified
+        cards[1] = cards[0]; // Wrong! String literals cannot be modified
+        cards[0] = cards[2]; // Wrong! String literals cannot be modified
+        cards[2] = cards[1]; // Wrong! String literals cannot be modified
+        cards[1] = a_card;   // Wrong! String literals cannot be modified
+
+        puts(cards);
+
+        return 0;
+}
