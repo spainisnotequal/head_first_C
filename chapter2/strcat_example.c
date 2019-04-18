4d00c2fda8accbec6fbe1507f33f63b1077643b7
spainisnotequal <spainisnotequal@gmail.com>
2019-04-18 09:10:24 +0200

    Modify variable's names

--- chapter2/strcat_example.c
+++ chapter2/strcat_example.c
@@ -8,43 +8,44 @@
 
 int main()
 {
-        char s1[20] = "news"; // We must allocate space in advance for the
-                              // composition of the two words
-        char s2[] = "paper";
+        char dest[20] = "news"; // We must allocate space in advance for the
+                                // composition of the two words
+        char src[] = "paper";
 
-        printf("The length of '%s' is: %zu\n", s1, strlen(s1));
-        printf("The length of '%s' is: %zu\n", s2, strlen(s2));
+        printf("The length of '%s' is: %zu\n", dest, strlen(dest));
+        printf("The length of '%s' is: %zu\n", src, strlen(src));
 
-        /* We cannot do the following, because s1 (and s2) would point to the
-           literal "news" (s2 to the literal "paper") in the constants memory
-           (which is read-only), so when we try to add s2 at the end of s1 we
-           try to write in the constants memory, which is not allowed:
-           char *s1 = "news";
-           char *s2 = "paper";
-           strcat(s1, s2); // WRONG!
+        /* We cannot do the following, because "dest" (and "src") would point to
+           the literal "news" ("src" to the literal "paper") in the constants
+           memory (which is read-only), so when we try to add "src" at the end
+           of "dest" we try to write in the constants memory, which is not
+           allowed:
+           char *dest = "news";
+           char *src = "paper";
+           strcat(dest, src); // WRONG!
         */
 
-        /* We cannot do the following neither, because s1 would not have enough
-           space (it only allocates 5 sizeof(char) bytes in the heap memory) to
-           add s2 at the end of s1, so we will be writing in an unknown part of
-           the heap memory (buffer overflow). Because of that possible buffer
-           overflow, the function says strccat() says that when the destination
-           array is not long enough to append the other string, the result is an
-           "undefined behaviour", and we should ALWAYS avoid that. Here it's the
-           example:
-           char s1[] = "news";
-           char s2[] = "paper";
-           strcat(s1, s2); // WRONG!
+        /* We cannot do the following neither, because "dest" would not have
+           enough space (it only allocates 5 sizeof(char) bytes in the heap
+           memory) to add "src" at the end of dest, so we will be writing in an
+           unknown part of the heap memory (buffer overflow). Because of that
+           possible buffer overflow, the function says strccat() says that when
+           the destination array is not long enough to append the other string,
+           the result is an "undefined behaviour", and we should ALWAYS avoid
+           that. Here it's the example:
+           char dest[] = "news";
+           char src[] = "paper";
+           strcat(dest, src); // WRONG!
         */
         
         // Before concatenating two strings we should always check that the
-        // string s1 have sufficient space to hold the result
-        size_t length1 = sizeof(s1)/sizeof(s1[0]);
+        // string dest have sufficient space to hold the result
+        size_t length1 = sizeof(dest)/sizeof(dest[0]);
 
-        if (length1 > strlen(s1) + strlen(s2)) {
-                strcat(s1, s2);
-                printf("The new compound word is: %s\n", s1);
-                printf("The length of '%s' is: %zu\n", s1, strlen(s1));
+        if (length1 > strlen(dest) + strlen(src)) {
+                strcat(dest, src);
+                printf("The new compound word is: %s\n", dest);
+                printf("The length of '%s' is: %zu\n", dest, strlen(dest));
 
         }
         else{
@@ -58,10 +59,9 @@ int main()
                    
            which appends not more than n characters (a null character and
            characters that follow it are not appended) from the array pointed to
-           by s2 to the end of the string pointed to by s1. The initial character
-           of s2 overwrites the null character at the end of s1
+           by "src" to the end of the string pointed to by "dest". The initial
+           character of "src" overwrites the null character at the end of "dest"
         */
-        
                     
         return 0;
 }
