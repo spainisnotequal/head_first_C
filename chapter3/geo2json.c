9726e3a27042bf3d4b7321c146ce80f79ba57d98
spainisnotequal <spainisnotequal@gmail.com>
2019-04-20 12:59:59 +0200

    Redirect error messages to the Standard Error

--- chapter3/geo2json.c
+++ chapter3/geo2json.c
@@ -21,11 +21,11 @@ int main ()
                 printf("{latitude: %f, longitude: %f, info: '%s'}", latitude,
                        longitude, info);
                 if ((latitude < - 90.0) || (latitude > 90.0)) {
-                        printf("\nInvalid latitude: %f\n", latitude);
+                        fprintf(stderr, "\nInvalid latitude: %f\n", latitude);
                         return 2;
                 }
                 if ((longitude < - 180.0) || (longitude > 180.0)) {
-                        printf("\nInvalid longitude: %f\n", longitude);
+                        fprintf(stderr, "\nInvalid longitude: %f\n", longitude);
                         return 2;
                 }
         }
