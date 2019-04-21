a46e0d62bb3dbce5bcc0636b373378f496a3f3bd
spainisnotequal <spainisnotequal@gmail.com>
2019-04-21 10:09:41 +0200

    Add safety checks to data streams (opening files)

--- chapter3/categorize.c
+++ chapter3/categorize.c
@@ -14,11 +14,30 @@ int main (int argc, char *argv[])
                 return 1;
         }
         
-        FILE *in = fopen("spooky.csv", "r");
-        FILE *file1 = fopen(argv[2], "w");
-        FILE *file2 = fopen(argv[4], "w");
-        FILE *file3 = fopen(argv[5], "w");
-
+        FILE *in;
+        if (!(in = fopen("spooky.csv", "r"))) {
+                fprintf(stderr, "Can't open the file\n");
+                return 1;
+        }
+        
+        FILE *file1;
+        if (!(file1 = fopen(argv[2], "w"))) {
+                fprintf(stderr, "Can't open the file '%s'\n", argv[2]);
+                return 1;
+        }
+        
+        FILE *file2;
+        if (!(file2 = fopen(argv[4], "w"))) {
+                fprintf(stderr, "Can't open the file '%s'\n", argv[4]);
+                return 1;
+        }
+        
+        FILE *file3;
+        if (!(file3 = fopen(argv[5], "w"))) {
+                fprintf(stderr, "Can't open the file '%s'\n", argv[5]);
+                return 1;
+        }
+        
         while (fscanf(in, "%79[^\n]\n", line) == 1) {
                 if(strstr(line, argv[1]))
                         fprintf(file1, "%s\n", line);
