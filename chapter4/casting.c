60ebf9ba4b298e6533ecff31a67ca97a00fa154f
spainisnotequal <spainisnotequal@gmail.com>
2019-04-21 19:46:09 +0200

    Casting a floating point number to an interger is a very bad idea

--- chapter4/casting.c
+++ chapter4/casting.c
@@ -17,19 +17,33 @@ int main ()
                      // smaller one
         printf("The value of z is %hi\n", z); // the result is -31072
 
-        /* Explicit casting between integers and floating point numbers */
+        /* Implicit casting integers to floating point numbers */
         int a = 7;
         int b = 2;
         float c = a / b;
         printf("a / b = %f\n", c); // Wrong!, we are performing an integer
                                    // division
 
+        /* Explicit casting integers to floating point numbers */
         float d = (float)a / (float)b;
         printf("a / b = %f\n", d); // Correct!
 
         float e = (float)a / b;
         printf("a / b = %f\n", e); // Correct!, the compiler will automatically
                                    // cast b to a float point number
+
+        /* Implicit casting floating point numbers to integers */
+        float f = 83.6598;
+        int g = f;
+        printf("The value of g is %i\n", g); // Wrong!, we are loosing the
+                                             // decimal part of the floating
+                                             // point number
+        
+        /* Explicit casting floating point numbers to integers */
+        int h = (int)f;
+        printf("The value of h is %i\n", h); // Wrong!, we are loosing the
+                                             // decimal part of the floating
+                                             // point number
         
         return 0;
 }
