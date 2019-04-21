d8ed28081ad372e50074907ec2b3f4f41c2d84eb
spainisnotequal <spainisnotequal@gmail.com>
2019-04-21 20:47:50 +0200

    Check the limits of different data types

--- chapter4/datatypes_size.c
+++ chapter4/datatypes_size.c
@@ -1,39 +1,38 @@
 /*
- * Program to print the size of different data types
+ * Program to print the limits and size of different data types
  */
 
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
 
 int main ()
 {
         //Integers
-        char  a;
-        short b;
-        int   c;
-        long  d;
-
-        printf("\nThe size of the interger 'char' data type is:  %zu bytes\n", sizeof a);
-        printf("The size of the interger 'short' data type is: %zu bytes\n", sizeof b);
-        printf("The size of the interger 'int' data type is:   %zu bytes\n", sizeof c);
-        printf("The size of the interger 'long' data type is:  %zu bytes\n", sizeof d);
-
-        //Floating points
-        float  e;
-        double f;
+        printf("\nThe size of the interger 'char' data type is: %zu bytes\n", sizeof(char));
+        printf("The minimum value of a 'char' is %i\n", CHAR_MIN);
+        printf("The maximum value of a 'char' is %i\n", CHAR_MAX);
         
-        printf("\nThe size of the floating point 'float' data type is:  %zu bytes\n", sizeof e);
-        printf("The size of the floating point 'double' data type is: %zu bytes\n", sizeof f);
-
-        // Other numbers: unsigned, long
-        unsigned int g;
-        long int h;
-        long long i;
-        long double j;
-
-        printf("\nThe size of the interger 'unsigned int' data type is:      %zu bytes\n", sizeof g);
-        printf("The size of the interger 'long int' data type is:          %zu bytes\n", sizeof h);
-        printf("The size of the interger 'long long' data type is:         %zu bytes\n", sizeof i);
-        printf("The size of the floating point 'long double' data type is: %zu bytes\n", sizeof j);
+        printf("\nThe size of the interger 'short' data type is: %zu bytes\n", sizeof(short));
+        printf("The minimum value of a 'short' is %hi\n", SHRT_MIN);
+        printf("The maximum value of a 'short' is %hi\n", SHRT_MAX);
+        
+        printf("\nThe size of the interger 'int' data type is: %zu bytes\n", sizeof(int));
+        printf("The minimum value of a 'int' is %i\n", INT_MIN);
+        printf("The maximum value of a 'int' is %i\n", INT_MAX);
+        
+        printf("\nThe size of the interger 'long' data type is: %zu bytes\n", sizeof(long));
+        printf("The minimum value of a 'long' is %li\n", LONG_MIN);
+        printf("The maximum value of a 'long' is %li\n", LONG_MAX);
+        
+        //Floating points
+        printf("\nThe size of the floating point 'float' data type is:  %zu bytes\n", sizeof(float));
+        printf("The minimum value of a 'float' is %.50f\n", FLT_MIN);
+        printf("The maximum value of a 'float' is %f\n", FLT_MAX);
+        
+        printf("\nThe size of the floating point 'double' data type is: %zu bytes\n", sizeof(double));
+        printf("The minimum value of a 'double' is %.50f\n", DBL_MIN);
+        printf("The maximum value of a 'double' is %f\n", DBL_MAX);
         
         return 0;
 }
