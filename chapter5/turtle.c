e31b5a6b4748a679a255e24966aaf8baa55cd451
spainisnotequal <spainisnotequal@gmail.com>
2019-04-27 16:46:23 +0200

    In C, parameters are passed to functions by value

---
+++ chapter5/turtle.c
@@ -0,0 +1,28 @@
+/*
+ * Program to learn about passing structures to functions and how pointers
+ * to structures work
+ */
+
+#include <stdio.h>
+
+struct turtle {
+        const char *name;
+        const char *species;
+        int age;
+};
+
+void happy_birthday (struct turtle *t)
+{
+        (*t).age = (*t).age + 1;
+        printf("Happy Birthday %s! You are now %i years old!\n",
+               (*t).name, (*t).age);
+}
+
+int main()
+{
+        struct turtle myrtle = {"Myrtle", "Leatherback sea turtle", 99};
+        happy_birthday(&myrtle);
+        printf("%s's age is now %i\n", myrtle.name, myrtle.age);
+        
+        return 0;
+}
