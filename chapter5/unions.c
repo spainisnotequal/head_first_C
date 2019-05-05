e504167d294f6b95bae5f4e06bc251fde2a03202
spainisnotequal <spainisnotequal@gmail.com>
2019-05-05 17:45:11 +0200

    Add a function to print the union's bytes using big-endian notation

--- chapter5/unions.c
+++ chapter5/unions.c
@@ -8,20 +8,36 @@ union quantity {
         short count;
         float weight;
         float volume;
+        
+        unsigned char bytes[sizeof(float)]; // for printing purposes
 };
+
+/*
+ * show_bytes prints bytes using big-endian notation. On a little-endian system,
+ * the bytes are going to be stored backwards from what you are used to seeing
+ */
+void show_bytes(union quantity q) 
+{
+        for(size_t i = 0; i < sizeof(float); i++)
+                printf(" %x", q.bytes[i]);
+        printf("\n");
+}
 
 int main()
 {        
         union quantity q1 = {9}; // Not the best way to initialize a union
                                  // (or a structure)
+        show_bytes(q1);
 
         union quantity q2 = { // Much better way to initialize a union or a
                               // structure
                 .weight = 3.3
         };
+        show_bytes(q2);
         
         union quantity q3;
         q3.weight = 3.3; // Also a great way to initialize a union or a structure
+        show_bytes(q3);
 
         // Be aware that by using the C89 style, we can only initialize the first
         // field, and by not explicitely writing the field name, it's easier to
