321dea6e84457147ffff438ddc73a269e72e124a
spainisnotequal <spainisnotequal@gmail.com>
2019-05-10 12:44:15 +0200

    Program to filter an array using string functions

---
+++ chapter7/find.c
@@ -0,0 +1,36 @@
+/*
+ * Program to learn about passing functions as parameters to other functions
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+size_t NUM_ADS = 7;
+const char *ADS[] = {
+        "William: SBM GSOH likes sports, TV, dining",
+        "Matt: SWM NS likes art, movies, theater",
+        "Luis: SLM ND likes books, theater, art",
+        "Mike: DWM DS likes trucks, sports and bieber",
+        "Peter: SAM likes chess, working out and art",
+        "Josh: SJM likes sports, movies and theater",
+        "Jed: DBM likes theater, books and dining"
+};
+
+void find ()
+{
+        puts("Search results:");
+        puts("------------------------------------------");
+
+        for (size_t i = 0; i < NUM_ADS; i++) {
+                if ((strstr(ADS[i], "sports")) && (! strstr(ADS[i], "bieber")))
+                        printf("%s\n", ADS[i]);
+        }
+        puts("------------------------------------------");
+}
+
+int main()
+{
+        find();
+               
+        return 0;
+}
