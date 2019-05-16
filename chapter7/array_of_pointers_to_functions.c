66455b1d7706da8346a3a1754ba2eead2267e0c2
spainisnotequal <spainisnotequal@gmail.com>
2019-05-16 09:32:52 +0200

    Add another case (enum and function) to the example

--- chapter7/array_of_pointers_to_functions.c
+++ chapter7/array_of_pointers_to_functions.c
@@ -5,43 +5,53 @@
 #include <stdio.h>
 
 
-enum response_type {DUMP, SECOND_CHANCE, MARRIAGE};
+enum response_type {DUMP, SECOND_CHANCE, MARRIAGE, LAW_SUIT};
 
 struct response {
         char *name;
         enum response_type type;
 };
 
-void dump(struct response r)
+void dump (struct response r)
 {
         printf("Dear %s,\n", r.name);
         puts("Unfortunately your last date contacted us to");
         puts("say that they will not be seeing you again");
 }
 
-void second_chance(struct response r)
+void second_chance (struct response r)
 {
         printf("Dear %s,\n", r.name);
         puts("Good news: your last date has asked us to");
         puts("arrange another meeting. Please call ASAP.");
 }
 
-void marriage(struct response r)
+void marriage (struct response r)
 {
         printf("Dear %s,\n", r.name);
         puts("Congratulations! Your last date has contacted");
         puts("us with a proposal of marriage.");
 }
 
-void (*replies[])(struct response) = {dump, second_chance, marriage};
+void law_suit (struct response r)
+{
+        printf("Dear %s,\n", r.name);
+        puts("I'm sorry, but you are going to court");
+        puts("because someone didn't like your catcall.");
+}
+
+void (*replies[])(struct response) = {dump, second_chance, marriage, law_suit};
 
 int main (void)
 {
         struct response r[] = {
                 {"Mike", DUMP}, {"Luis", SECOND_CHANCE},
-                {"Matt", SECOND_CHANCE}, {"William", MARRIAGE}
+                {"Matt", SECOND_CHANCE}, {"William", MARRIAGE},
+                {"George", LAW_SUIT}
         };
-        for (size_t i = 0; i < 4; i++) {
+        size_t  num_elements = sizeof(r)/sizeof(r[0]);
+        
+        for (size_t i = 0; i < num_elements; i++) {
                 (replies[r[i].type])(r[i]);
         }
 
