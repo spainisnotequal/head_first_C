40d7844042f643c94572fbbc3f282d9999ff29ef
spainisnotequal <spainisnotequal@gmail.com>
2019-05-16 09:20:51 +0200

    Delete switch block and use an array of pointers to functions instead

--- chapter7/array_of_pointers_to_functions.c
+++ chapter7/array_of_pointers_to_functions.c
@@ -33,6 +33,7 @@ void marriage(struct response r)
         puts("us with a proposal of marriage.");
 }
 
+void (*replies[])(struct response) = {dump, second_chance, marriage};
 
 int main (void)
 {
@@ -41,16 +42,7 @@ int main (void)
                 {"Matt", SECOND_CHANCE}, {"William", MARRIAGE}
         };
         for (size_t i = 0; i < 4; i++) {
-                switch (r[i].type) {
-                case DUMP:
-                        dump(r[i]);
-                        break;
-                case SECOND_CHANCE:
-                        second_chance(r[i]);
-                        break;
-                default:
-                        marriage(r[i]);
-                }
+                (replies[r[i].type])(r[i]);
         }
 
         return 0;
