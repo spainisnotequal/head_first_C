81df938fcbaa0b7967bbe893727e90756c2c6c6e
spainisnotequal <spainisnotequal@gmail.com>
2019-05-28 09:21:17 +0200

    Example about system calls

---
+++ chapter9/guard_log.c
@@ -0,0 +1,27 @@
+/*
+ * Program to learn about system calls
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+char * now (void)
+{
+        time_t t;
+        time (&t);
+        return asctime(localtime (&t));
+}
+
+int main (void)
+{
+        char comment[80];
+        char cmd[120];
+        
+        fgets(comment, 80, stdin);
+        sprintf(cmd, "echo '%s %s' >> reports.log", comment, now());
+        system(cmd);
+
+        return 0;
+}
