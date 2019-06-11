fec00e7740a1e74771a16e2a37c1d33be12505dc
spainisnotequal <spainisnotequal@gmail.com>
2019-06-11 10:48:57 +0200

    Add a function to handle shutdown situations

--- chapter11/ikk_server.c
+++ chapter11/ikk_server.c
@@ -76,3 +76,15 @@ int read_in(int socket, char *buf, int len)
 
         return len -slen;
 }
+
+int listener_d;
+
+void handle_shutdown(int sig)
+{
+        if (listener_d) {
+                close(listener_d);
+        }
+
+        fprintf(stderr, "Bye!\n");
+        exit(0);
+}
